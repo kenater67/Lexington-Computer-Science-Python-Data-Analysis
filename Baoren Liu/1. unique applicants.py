47f87ca0810bac2ee6e151537921db40683bb8d3
Robotic Reaper <64423919+RoboticReaper@users.noreply.github.com>
2020-12-28 14:38:32 -0800

    Add files via upload

--- Baoren Liu/1. unique applicants.py
+++ Baoren Liu/1. unique applicants.py
@@ -1,10 +1,10 @@
 import csv
 
-unique = []
+unique = set([])
 with open("data.csv", "r") as file:
     plots = csv.reader(file, delimiter=",")
     for row in plots:
         if row[0] not in unique:
-            unique.append(row[0])
+            unique.add(row[0])
 
 print(len(unique))
