bb78af239476c949580e09ea52a10f569472ae0b
lindachen1 <63442206+lindachen1@users.noreply.github.com>
2021-01-20 09:12:30 -0500

    Update CELquestions.py

--- Linda Chen/CELquestions.py
+++ Linda Chen/CELquestions.py
@@ -1,6 +1,5 @@
 import csv 
 import matplotlib.pyplot as plt
-import numpy as np
   
 filename = "CEL_edited.csv"
   
@@ -29,6 +28,7 @@ with open(filename, 'r') as csvfile:
         # applications per year, category
         numApps[int(row[1])-2015] += 1
 
+# display data
 print("Unique applicants:", len(uniqueApp))
 print("Number reapplied:", len(duplicates))
 
@@ -36,57 +36,39 @@ year = [2015, 2016, 2017, 2018, 2019, 2020]
 category = ["Ecological Well-being", "Health & Human Services", "Arts & Culture", "Community Building", "Environment"]
 
 print("\nPer year, category: money granted and percentage fulfilled")
-years = range(6)
-categories = range(5)
-for i in years:
-    for j in categories:
+for i in range(6):
+    for j in range(5):
         if moneyRequested[i][j] == 0:
             print(i+2015,",",j,":",moneyGranted[i][j],", 0.0%")
         else:
             perFull[i][j] = round((moneyGranted[i][j] / moneyRequested[i][j])*100, 2)
             print(i+2015,",",j,":",moneyGranted[i][j],",", perFull[i][j],"%")
-for i in years:
+for i in range(6):
     graphTitle = "Percentage fulfilled for each category in " + str(i+2015) 
     plt.title(graphTitle) 
     plt.bar(category, perFull[i])   
     plt.show()    
 
-moneyPerYear = [0] * 6
 print("\nMoney granted per year:")
-for i in years:
-    x = sum(moneyGranted[i])
-    print(2015+i,":",x)
-    moneyPerYear[i] = x
+moneyPerYear = [sum(value) for value in (moneyGranted)]
+for i in range(6):
+    print(2015+i,":",moneyPerYear[i])
 plt.title("Money granted in each year")
 plt.bar(year, moneyPerYear)
 plt.show()
     
-moneyPerCategory = [0] * 5
 print("\nMoney granted per category:")
-highest = 0
-maxCategory = ""
-for j in categories:
-    x = 0
-    for i in years:
-        x += moneyGranted[i][j]
-    print(j,":",x)
-    if x > highest:
-        highest = x
-        maxCategory = j
-    moneyPerCategory[j] = x
-print("Category", maxCategory, "received the most money.")
+moneyPerCategory = [sum(value) for value in zip(*moneyGranted)]
+for j in range(5):
+    print(j,moneyPerCategory[j])
+print("Category", moneyPerCategory.index(max(moneyPerCategory)), "received the most money.")   
 plt.title("Money granted to each category")
 plt.bar(category, moneyPerCategory)
 plt.show()
 
-appsPerYear = [0] * 6
-
 print("\nApplicants per year:")
-for i in years:
-    total = 0
-    total += numApps[i]
-    print(2015+i,":",total)
-    appsPerYear[i] = total
+for i in range(6):
+    print(2015+i,":",numApps[i])
 plt.title("Number of applicants in each year")
 plt.bar(year, numApps)
 plt.show()
