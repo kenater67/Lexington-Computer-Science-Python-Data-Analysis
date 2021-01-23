3f84a62b3c53cf7611f1a3c2636f5f14b0fb8d32
mgmgperson <albert1104@gmail.com>
2021-01-23 15:09:15 -0500

    Update q4.py

--- Albert Lin/py/q4.py
+++ Albert Lin/py/q4.py
@@ -5,19 +5,9 @@ ysince15=[0,0,0,0,0,0]
 with open("celdata2.csv","r") as file:
 	requests=csv.reader(file,delimiter=",")
 	for x in requests:
-		if x[1][0:4]=="2015":
-			ysince15[0]+=1
-		if x[1][0:4]=="2016":
-			ysince15[1]+=1
-		if x[1][0:4]=="2017":
-			ysince15[2]+=1
-		if x[1][0:4]=="2018":
-			ysince15[3]+=1
-		if x[1][0:4]=="2019":
-			ysince15[4]+=1
-		if x[1][0:4]=="2020":
-			ysince15[5]+=1
+		ysince15[int(x[1][0:4])-2015]+=1
 yr=2015
 for y in ysince15:
 	print(str(yr)+": "+str(y))
 	yr+=1
+#only works if uses file with removed first row
