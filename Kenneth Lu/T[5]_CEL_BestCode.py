7f2ce52858880e8ab6b7742941a7b56d30148864
kenater67 <67668219+kenater67@users.noreply.github.com>
2021-02-12 20:12:57 -0500

    Add files via upload

--- Kenneth Lu/T[5]_CEL_BestCode.py
+++ Kenneth Lu/T[5]_CEL_BestCode.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import csv
 
 category = ["Ecological Well-being", "Health & Human Services", "Arts & Culture", "Community Building", "Environment"]
+arr = np.empty((0, 6), str)
 moneyGranted = [[0]*5 for _ in range(6)]
 moneyRequested = [[0]*5 for _ in range(6)]
 perFull = [[0]*5 for _ in range(6)]
@@ -10,6 +11,11 @@ perFull = [[0]*5 for _ in range(6)]
 
 def task5(arr):  # function definition; be sure to add your task number after 'task'
     # Write your code here
+
+    for row in arr:
+        moneyGranted[int(row[1])-2015][int(row[3])-1] += int(row[4])
+        moneyRequested[int(row[1])-2015][int(row[3])-1] += int(row[5]) 
+    
     for i in range(6):
         for j in range(5):
             if moneyRequested[i][j] == 0:
@@ -21,17 +27,16 @@ def task5(arr):  # function definition; be sure to add your task number after 't
         graphTitle = "Percentage fulfilled for each category in " + str(i+2015) 
         plt.title(graphTitle) 
         plt.bar(category, perFull[i])   
-        plt.show() 
+        plt.show()  
 
     
 
 with open('CEL_HistoricalGrantInformation_2014-7Oct2020_CSV.csv', newline='') as csvfile:  # reading the csv file
     reader = csv.DictReader(csvfile)
     for row in reader:
-        moneyGranted[int(row["year_id"])-2015][int(row["area_id"])-1] += int(row["awarded_id"])
-        moneyRequested[int(row["year_id"])-2015][int(row["area_id"])-1] += int(row["requested_id"]) 
-        #arr = np.append(arr, np.array([[row['organization_id'], int(row['year_id']), row['process_id'],
-        #                               int(row['area_id']), int(row['awarded_id']), int(row['requested_id'])]]), axis=0)
+        arr = np.append(arr, np.array([[row['organization_id'], int(row['year_id']), row['process_id'],
+                                       int(row['area_id']), int(row['awarded_id']), int(row['requested_id'])]]), axis=0)
+
     #print(arr)
 
-task5(perFull)  # function call; be sure to add your task number after 'task'
+task5(arr)
