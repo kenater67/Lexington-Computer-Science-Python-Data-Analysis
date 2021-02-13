d0b739786ce26381d1f25430a6731aeb6e603428
everestyang1 <75863691+everestyang1@users.noreply.github.com>
2021-02-12 22:13:58 -0500

    Add files via upload

--- Everest Yang/T6_CEL_BestCode.py
+++ Everest Yang/T6_CEL_BestCode.py
@@ -1,14 +1,24 @@
 import csv
+import numpy as np
+
+arr = np.empty((0, 6), str)
 
 applicants = set([])
 reapplied = set([])
-with open("C:\\CEL\\CELdata.csv", "r") as file:
-    plots = csv.reader(file, delimiter=",")
-    for row in plots:
+
+def task6(arr):
+    for row in arr:
         if row[0] not in applicants:
             applicants.add(row[0])
         else:
             reapplied.add(row[0])
-
 txt = "{} applicants reapplied"
 print(txt.format(len(reapplied)))
+
+
+with open('CEL_HistoricalGrantInformation_2014-7Oct2020_CSV.csv', "r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        arr = np.append(arr, np.array([[row['organization_id'], int(row['year_id']), row['process_id'], int(row['area_id']), int(row['awarded_id']), int(row['requested_id'])]]), axis=0)
+
+task6(arr)
